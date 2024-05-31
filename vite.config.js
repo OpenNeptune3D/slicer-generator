e98e78e0df98fb4486c8008c53147576698c5bb3
Phillip Thelen <phillip@thelen.space>
2024-05-31 11:26:52 +0200

    generate sourcemap

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/slicer-generator/',
+	build: {
+		sourcemap: true,
+	},
 	plugins: [
 		preact({
 			prerender: {
