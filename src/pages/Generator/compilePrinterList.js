ca473a2801e5ded4039c2efc3920ae336df81a07
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-08-08 21:45:39 +0100

    Update compilePrinterList.js

--- src/pages/Generator/compilePrinterList.js
+++ src/pages/Generator/compilePrinterList.js
@@ -1,11 +1,17 @@
-export default function compilePrinterList() {
-    const printers = import.meta.glob('../../profiles/printers/*.json');
-    return  Promise.all(Object.keys(printers).map(async (key) => {
-        const profile = await printers[key]();
-        return {
-            "name": profile.name.replace("-OpenNept4une", ""),
-            "identifier": profile.name,
-            "profile": profile
-        }
-    }));
+export default async function compilePrinterList() {
+    try {
+        const printers = import.meta.glob('../../profiles/printers/*.json');
+        const printerList = await Promise.all(Object.keys(printers).map(async (key) => {
+            const profile = await printers[key]();
+            return {
+                name: profile.name.replace("-OpenNept4une", ""),
+                identifier: key.split('/').pop().replace('.json', ''), // Use the file name without the extension as identifier
+                profile: profile
+            };
+        }));
+        return printerList;
+    } catch (error) {
+        console.error("Error compiling printer list:", error);
+        throw error;
+    }
 }
