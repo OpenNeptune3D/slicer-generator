4bca19b2521b2b4e21b4faf07691b5cce2769154
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-08-08 21:45:00 +0100

    Update compileFilamentList.js

--- src/pages/Generator/compileFilamentList.js
+++ src/pages/Generator/compileFilamentList.js
@@ -1,11 +1,17 @@
-export default function compileFilamentList() {
-    const filament = import.meta.glob('../../profiles/filaments/*.json');
-    return  Promise.all(Object.keys(filament).map(async (key) => {
-        const profile = await filament[key]();
-        return {
-            "name": profile.name.replace("-OpenNept4une", ""),
-            "identifier": profile.name,
-            "profile": profile
-        }
-    }));
+export default async function compileFilamentList() {
+    try {
+        const filaments = import.meta.glob('../../profiles/filaments/*.json');
+        const filamentList = await Promise.all(Object.keys(filaments).map(async (key) => {
+            const profile = await filaments[key]();
+            return {
+                name: profile.name.replace("-OpenNept4une", ""),
+                identifier: key.split('/').pop().replace('.json', ''), // Use the file name without the extension as identifier
+                profile: profile
+            };
+        }));
+        return filamentList;
+    } catch (error) {
+        console.error("Error compiling filament list:", error);
+        throw error;
+    }
 }
