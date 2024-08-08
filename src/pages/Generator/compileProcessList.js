b060b79e6f9a6526984840a9d8dc8a090dddd320
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-08-08 21:29:13 +0100

    Update compileProcessList.js

--- src/pages/Generator/compileProcessList.js
+++ src/pages/Generator/compileProcessList.js
@@ -1,11 +1,17 @@
-export default function compileProcessList() {
-    const processes = import.meta.glob('../../profiles/processes/*.json');
-    return  Promise.all(Object.keys(processes).map(async (key) => {
-        const profile = await processes[key]();
-        return {
-            "name": profile.name.replace("-OpenNept4une", ""),
-            "identifier": profile.name,
-            "profile": profile
-        }
-    }));
+export default async function compileProcessList() {
+    try {
+        const processes = import.meta.glob('../../profiles/processes/*.json');
+        const processList = await Promise.all(Object.keys(processes).map(async (key) => {
+            const profile = await processes[key]();
+            return {
+                name: profile.name.replace("-OpenNept4une", ""),
+                identifier: profile.name,
+                profile: profile
+            };
+        }));
+        return processList;
+    } catch (error) {
+        console.error("Error compiling process list:", error);
+        throw error;
+    }
 }
