cbd608653e19b50fa548c4b63662af0c0595f6f2
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-08-30 12:22:43 +0100

    Update createZip.js

--- src/pages/Generator/createZip.js
+++ src/pages/Generator/createZip.js
@@ -1,24 +1,28 @@
 import JSZip from "jszip";
 
 function addFile(zip, type, file) {
-    let path = type + "/" + file.name
-        if (path.indexOf(".json") === -1) {
-            path += ".json";
-        }
-        zip.file(path, file.content);
+    let path = type + "/" + file.name;
+    if (path.indexOf(".json") === -1) {
+        path += ".json";
+    }
+    console.log(`Adding file to zip: ${path} with content:`, file.content); // Log file content
+    zip.file(path, JSON.stringify(file.content, null, 2)); // Ensure content is correctly stringified
 }
 
 export const createZip = async (printer_profiles, filament_profiles, process_profiles) => {
     const zip = new JSZip();
-    console.log(printer_profiles, filament_profiles, process_profiles);
+
     printer_profiles.forEach((file) => {
         addFile(zip, "printers", file);
     });
+
     filament_profiles.forEach((file) => {
         addFile(zip, "filaments", file);
     });
+
     process_profiles.forEach((file) => {
         addFile(zip, "processes", file);
     });
+
     return zip.generateAsync({ type: "blob" });
 };
