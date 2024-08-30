ce24a77ea7bdf1cddda3e8afb397ce6d6deae960
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-08-30 13:48:36 +0100

    Update createZip.js

--- src/pages/Generator/createZip.js
+++ src/pages/Generator/createZip.js
@@ -1,26 +1,29 @@
 import JSZip from "jszip";
 
 function addFile(zip, type, file) {
-    let path = type + "/" + file.name;
-    if (path.indexOf(".json") === -1) {
+    let path = `${type}/${file.name}`;
+    if (!path.endsWith(".json")) {
         path += ".json";
     }
-    console.log(`Adding file to zip: ${path} with content:`, file.content); // Log file content to verify it is not empty
-    zip.file(path, JSON.stringify(file.content, null, 2)); // Ensure content is correctly stringified
+    console.log(`Adding file to zip: ${path} with content:`, file); // Log file content to verify it is not empty
+    zip.file(path, JSON.stringify(file, null, 2)); // Ensure content is correctly stringified
 }
 
 export const createZip = async (printer_profiles, filament_profiles, process_profiles) => {
     const zip = new JSZip();
 
-    printer_profiles.forEach((file) => {
+    printer_profiles.forEach((profile) => {
+        const file = { name: profile.name, content: profile };
         addFile(zip, "printers", file);
     });
 
-    filament_profiles.forEach((file) => {
+    filament_profiles.forEach((profile) => {
+        const file = { name: profile.name, content: profile };
         addFile(zip, "filaments", file);
     });
 
-    process_profiles.forEach((file) => {
+    process_profiles.forEach((profile) => {
+        const file = { name: profile.name, content: profile };
         addFile(zip, "processes", file);
     });
 
